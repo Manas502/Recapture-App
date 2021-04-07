5d0bc891f2951a078058547fcb3caa1e37af7f9a
Manas502 <manasdeep.02@gmail.com>
2021-04-07 11:32:01 +0530

    initial commit

---
+++ client/src/components/Form/styles.js
@@ -0,0 +1,26 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(2),
+    },
+  },
+  paper: {
+    marginTop: '150px',
+    padding: theme.spacing(3),
+  },
+  form: {
+    marginTop: '0px',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  fileInput: {
+    width: '97%',
+    margin: '10px 0',
+  },
+  buttonSubmit: {
+    marginBottom: 10,
+  },
+}));
